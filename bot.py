c978efa74cd10f9c5c85bef5fe65c6cc9cbbbb94
Joshua Kenyon (kenyonj) <kenyonj@coventry.ac.uk>
2018-10-24 15:47:22 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,28 @@
 #Get user input
+import discord
 
+TOKEN = 'NTA0NjYwOTQ5OTcwNzE0NjQ1.DrISJw.jP8hJ23eycSshKKa7ur-w0ylkwc'
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!hello'):
+        msg = 'Hello {0.author.mention}'.format(message)
+        await client.send_message(message.channel, msg)
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+client.run(TOKEN)
 #Pass into getKeyword() which breaks down the sentence and determines
 #what module should be ran.
 
