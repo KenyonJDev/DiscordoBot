abfcdafd0287409d8a1f1061b6b47981cefa75e1
Brad Angliss <anglissb@coventry.ac.uk>
2018-10-24 19:41:45 +0000

    Added comments to describe bot.py. Rearranged code. Added a loop that takes the user input and splits it into an array for processing keywords

--- bot.py
+++ bot.py
@@ -12,16 +12,23 @@ async def on_ready():
     print(client.user.id)
     print('------')
     
+    
 @client.event
 async def on_message(message):
+    
     # we do not want the bot to reply to itself
     if message.author == client.user:
         return
+    
+    stringArr = message.content.split(" ") #Split the input into an array
+    
+    #Loop through array, currently outputs back to user one by one
+    #But this is where you would check for keywords
+    
+    for i in range(0,len(stringArr)):
+        await client.send_message(message.channel, stringArr[i])
 
-    if message.content.startswith('!hello'):
-        msg = 'Hello {0.author.mention}'.format(message)
-        await client.send_message(message.channel, msg)
-        
+    #Tells the user how many messages are in the chat log between them and the bot
     if message.content.startswith('!test'):
         counter = 0
         tmp = await client.send_message(message.channel, 'Calculating messages...')
@@ -30,11 +37,16 @@ async def on_message(message):
                 counter += 1
         await client.edit_message(tmp, 'You have {} messages.'.format(counter))
         
+    #After 5 seconds the bot 'wakes up'
     elif message.content.startswith('!sleep'):
         await asyncio.sleep(5)
         await client.send_message(message.channel, 'Done sleeping')
-    
-    
+        
+    #Greets the user if they type !hello
+    elif message.content.startswith('!hello'):
+        msg = 'Hello {0.author.mention}'.format(message)
+        await client.send_message(message.channel, msg)
+        
 client.run(TOKEN)
 
 #Pass into getKeyword() which breaks down the sentence and determines
