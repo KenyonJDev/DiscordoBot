263364fa7294663e2ea7a883a1c1511a5e743af5
Brad Angliss <anglissb@coventry.ac.uk>
2018-10-24 13:48:42 +0100

    Add math.py, instructions in bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,9 @@
+#Get user input
 
+#Pass into getKeyword() which breaks down the sentence and determines
+#what module should be ran.
+
+#For example:
+#User enters "What is 2 x 2?"
+#getKeyword("What is 2 x 2?")
+#getKeyword return that math.py should be used
