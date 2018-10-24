3862fea419d0256cb48c1e72585f9ce11891a424
Joshua Kenyon (kenyonj) <kenyonj@coventry.ac.uk>
2018-10-24 14:22:01 +0100

    Update weather.py

--- weather.py
+++ weather.py
@@ -3,6 +3,9 @@ import urllib.parse
 import urllib.request
 import json
 
+### Appkey = Zw49WH32
+### Query input: select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="Coventry, UK")
+
 baseurl = "https://query.yahooapis.com/v1/public/yql?"
 yql_query = "select wind from weather.forecast where woeid=2460286"
 yql_url = baseurl + urllib.parse.urlencode({'q':yql_query}) + "&format=json"
@@ -10,4 +13,3 @@ result = urllib.request.urlopen(yql_url).read()
 data = json.loads(result)
 print data['query']['results']
 
-### Query input: select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="Coventry, UK")
