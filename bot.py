abee8ece18035f6ea9bf267c8e4161e0d043b5fa
Joshua Kenyon (kenyonj) <kenyonj@coventry.ac.uk>
2018-11-06 20:07:33 +0000

    praying

--- bot.py
+++ bot.py
@@ -87,7 +87,7 @@ async def on_message(message):
     elif message.content.startswith('!weather'):
         weather.url_builder(stringInp)
         reply = rs.reply("localuser", stringInp)
-        await client.send_message(message.channel, s.format(**data))
+        await client.send_message(message.channel, data_output)
     else:
         reply = (rs.reply("localuser", "get database data")).split(" ")
         dbQueries.updateDB(userID,reply)
