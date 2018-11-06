ae998361e56341370cf2b0170fc7d95f305f7a93
Joshua Kenyon <kenyonj@coventry.ac.uk>
2018-11-06 10:11:09 +0000

    added error command

--- timetable.py
+++ timetable.py
@@ -7,7 +7,7 @@ def url_builder(location,transport):
     app_id = 'd1a1cdb4'
     api = 'http://transportapi.com/v3/uk/places.json?query='
 
-    full_url = api + str(location)+'&type=' + str(transport)+'&app_id=' + app_id + '&app_key='+ bd5f386fb6fbac9344fc1c4e41f48002
+    full_url = api + str(location)+'&type=' + str(transport)+'&app_id=' + app_id + '&app_key='+ app_key
     return full_url
 
 # example URL: http://transportapi.com/v3/uk/places.json?query=euston&type=train_station&app_id=d1a1cdb4&app_key=bd5f386fb6fbac9344fc1c4e41f48002
