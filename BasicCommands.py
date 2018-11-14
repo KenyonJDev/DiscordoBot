26f34692c4eec0a548e5bb2f985b79666707ac6c
Joshua <kenyonJ@uni.coventry.ac.uk>
2018-11-14 12:28:40 +0000

    added loading code

--- BasicCommands.py
+++ BasicCommands.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-class commands:
+class BasicCommands:
     def __init__(self, client):
         self.client = client
 
@@ -14,16 +14,16 @@ class commands:
         fmt = '{0} joined on {0.joined_at} and has {1} roles.'
         await client.send(fmt.format(member, len(member.roles)))
 
-  #  @commands.command(pass_context=True)
-   # async def play(ctx):
-    #    url = ctx.message.content
-    #    author = ctx.message.author
-#
- #       voice_channel = author.voice_channel
-  #      vc = await client.join_voice_channel(voice_channel)
-#
- #       player = await vc.create_ytdl_player(url)
-  #      player.start()
+    @commands.command(pass_context=True)
+    async def play(ctx):
+        url = ctx.message.content
+        author = ctx.message.author
+
+        voice_channel = author.voice_channel
+        vc = await client.join_voice_channel(voice_channel)
+
+        player = await vc.create_ytdl_player(url)
+        player.start()
 
 
 
