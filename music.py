72085ad9e39bc94a2d67c2989b4d5d8f3511f640
Joshua Kenyon (kenyonj) <kenyonj@coventry.ac.uk>
2018-11-23 02:54:05 +0000

    Half assed attempt at music, will update tomorrow

--- music.py
+++ music.py
@@ -13,39 +13,6 @@ class music:
     @commands.command(aliases=['p','P'])
     @commands.guild_only()
     async def play(self, ctx, *, query: str):
-        """searches and plays a song"""
-        player = self.client.lavalink.players.get(ctx.guild.id)
-
-        query = query.strip('<>')
-
-        if not url_rx.match(query):
-            query = f'ytsearch:{query}'
-
-        results = await self.client.lavalink.get_tracks(query)
-
-        if not results or not results['tracks']:
-            return await ctx.send('Nothing found!')
-
-        embed = discord.Embed(color=discord.Color.blurple())
-
-        if results['loadType'] == 'PLAYLIST_LOADED':
-            tracks = results['tracks']
-
-            for track in tracks:
-                player.add(requester=ctx.author.id, track=track)
-
-            embed.title = 'Playlist Enqueued!'
-            embed.description = f'{results["playlistInfo"]["name"]} - {len(tracks)} tracks'
-            await ctx.send(embed=embed)
-        else:
-            track = results['tracks'][0]
-            embed.title = 'Track Enqueued'
-            embed.description = f'[{track["info"]["title"]}]({track["info"]["uri"]})'
-            await ctx.send(embed=embed)
-            player.add(requester=ctx.author.id, track=track)
-
-        if not player.is_playing:
-            await player.play()
 
 
 def setup(bot):
