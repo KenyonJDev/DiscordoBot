046938ed30f47602e5eff160c2b09a31edfc397e
Joshua Kenyon <99KenyonJ@gmail.com>
2020-01-14 23:08:07 +0000

    Commented out music.py until this script can be fixed

--- music.py
+++ music.py
@@ -1,19 +1,19 @@
-import discord
-from discord.ext import commands
+#import discord
+#from discord.ext import commands
 
-time_rx = re.compile('[0-9]+')
-url_rx = re.compile('https?:\/\/(?:www\.)?.+')
+#time_rx = re.compile('[0-9]+')
+#url_rx = re.compile('https?:\/\/(?:www\.)?.+')
 
-class music:
-    def __init__(self, client):
-        self.client = client
+#class music:
+    #def __init__(self, client):
+        #self.client = client
 
-    @commands.command(aliases=['p','P'])
-    @commands.guild_only()
-    async def play(self, ctx, *, query: str):
+    #@commands.command(aliases=['p','P'])
+    #@commands.guild_only()
+    #async def play(self, ctx, *, query: str):
 
 
 
-    def setup(bot):
-        bot.add_cog(music(bot))
-        print("music loaded")
+    #def setup(bot):
+     #   bot.add_cog(music(bot))
+      #  print("music loaded")
